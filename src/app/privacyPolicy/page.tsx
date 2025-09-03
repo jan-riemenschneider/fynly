28c938567f364e068148e983e89f1ecd284cae4f
jan-riemenschneider <jan-riemenschneider@outlook.de>
2025-09-03 09:15:20 +0200

    feat: implement neu desing system to the pages

--- src/app/privacyPolicy/page.tsx
+++ src/app/privacyPolicy/page.tsx
@@ -1,90 +1,94 @@
+import { Section } from '@/components/sections/Section'
+
 export default function PrivacyPolicy() {
   return (
-    <div className="container mx-auto flex flex-col justify-start gap-6 p-6">
-      <h1>Datenschutzerklärung (gemäß DSGVO)</h1>
+    <Section>
+      <div className=" col-span-12 flex flex-col justify-start gap-6 p-6">
+        <h1>Datenschutzerklärung (gemäß DSGVO)</h1>
 
-      <div className="space-y-2 md:space-y-4">
-        <h2>1. Allgemeine Hinweise</h2>
-        <p>
-          Der Schutz deiner persönlichen Daten ist uns wichtig. Nachfolgend
-          informieren wir dich über Art, Umfang und Zweck der Verarbeitung
-          personenbezogener Daten auf unserer Website und in unserem Shop.
-        </p>
+        <div className="space-y-2 md:space-y-4">
+          <h2>1. Allgemeine Hinweise</h2>
+          <p>
+            Der Schutz deiner persönlichen Daten ist uns wichtig. Nachfolgend
+            informieren wir dich über Art, Umfang und Zweck der Verarbeitung
+            personenbezogener Daten auf unserer Website und in unserem Shop.
+          </p>
+        </div>
+        <div className="space-y-2 md:space-y-4">
+          <h2>2. Verantwortliche Stelle</h2>
+          <p className="block">
+            fynly Lisa-Marie Aschoff Berliner Straße 62B 63477 Maintal
+            Deutschland E-Mail: folgt
+          </p>
+        </div>
+        <div className="space-y-2 md:space-y-4">
+          <h2>
+            3. Erhebung und Speicherung personenbezogener Daten sowie Art und
+            Zweck der Verwendung Beim Besuch unserer Website werden automatisch
+            Informationen durch deinen Browser an unseren Server gesendet. Dies
+            sind:
+          </h2>
+          <p>
+            IP-Adresse Datum und Uhrzeit der Anfrage Name und URL der
+            abgerufenen Datei Website, von der aus der Zugriff erfolgt
+            (Referrer-URL) verwendeter Browser und ggf. das Betriebssystem Diese
+            Daten werden zur Gewährleistung eines reibungslosen
+            Verbindungsaufbaus und zur Systemsicherheit verarbeitet.
+          </p>
+        </div>
+        <div className="space-y-2 md:space-y-4">
+          <h2>4. Datenverarbeitung bei Bestellungen</h2>
+          <p>
+            Wenn du über unseren Onlineshop einkaufst, erheben wir folgende
+            Daten: Vor- und Nachname Adresse ggf. Telefonnummer E-Mail-Adresse
+            Zahlungsdaten bestellte Produkte individuelle
+            Personalisierungswünsche (wenn angegeben) Diese Daten werden zur
+            Vertragserfüllung, Lieferung und Kundenkommunikation verwendet.
+          </p>
+        </div>
+        <div className="space-y-2 md:space-y-4">
+          <h2>5. Weitergabe von Daten</h2>
+          <p>
+            Deine Daten werden nur an Dritte weitergegeben, soweit dies zur
+            Abwicklung des Vertrages notwendig ist (z.B. Versanddienstleister,
+            Zahlungsdienstleister). Eine darüber hinausgehende Weitergabe
+            erfolgt nicht.
+          </p>
+        </div>
+        <div className="space-y-2 md:space-y-4">
+          <h2>6. Zahlungsdienstleister</h2>
+          <p>
+            Je nach gewählter Zahlungsart erfolgt die Datenverarbeitung ggf.
+            durch externe Anbieter (z.B. PayPal, Klarna, Stripe). Es gelten
+            deren Datenschutzrichtlinien.
+          </p>
+        </div>
+        <div className="space-y-2 md:space-y-4">
+          <h2>7. Cookies</h2>
+          <p>
+            Unsere Website verwendet Cookies, um die Nutzerfreundlichkeit zu
+            verbessern. Du kannst die Verwendung von Cookies in deinen
+            Browsereinstellungen steuern.
+          </p>
+        </div>
+        <div className="space-y-2 md:space-y-4">
+          <h2>8. Betroffenenrechte (DSGVO)</h2>
+          <p>
+            Du hast jederzeit das Recht auf: Auskunft über deine gespeicherten
+            Daten Berichtigung unrichtiger Daten Löschung („Recht auf
+            Vergessenwerden“) Einschränkung der Verarbeitung
+            Datenübertragbarkeit Widerspruch gegen die Verarbeitung Du kannst
+            dich jederzeit unter der oben genannten Adresse an uns wenden.
+          </p>
+        </div>
+        <div className="space-y-2 md:space-y-4">
+          <h2>9. Beschwerderecht bei der Aufsichtsbehörde</h2>
+          <p>
+            Du hast das Recht, dich bei einer Datenschutz-Aufsichtsbehörde über
+            die Verarbeitung deiner personenbezogenen Daten zu beschweren.
+          </p>
+        </div>
       </div>
-      <div className="space-y-2 md:space-y-4">
-        <h2>2. Verantwortliche Stelle</h2>
-        <p className="block">
-          fynly Lisa-Marie Aschoff Berliner Straße 62B 63477 Maintal Deutschland
-          E-Mail: folgt
-        </p>
-      </div>
-      <div className="space-y-2 md:space-y-4">
-        <h2>
-          3. Erhebung und Speicherung personenbezogener Daten sowie Art und
-          Zweck der Verwendung Beim Besuch unserer Website werden automatisch
-          Informationen durch deinen Browser an unseren Server gesendet. Dies
-          sind:
-        </h2>
-        <p>
-          IP-Adresse Datum und Uhrzeit der Anfrage Name und URL der abgerufenen
-          Datei Website, von der aus der Zugriff erfolgt (Referrer-URL)
-          verwendeter Browser und ggf. das Betriebssystem Diese Daten werden zur
-          Gewährleistung eines reibungslosen Verbindungsaufbaus und zur
-          Systemsicherheit verarbeitet.
-        </p>
-      </div>
-      <div className="space-y-2 md:space-y-4">
-        <h2>4. Datenverarbeitung bei Bestellungen</h2>
-        <p>
-          Wenn du über unseren Onlineshop einkaufst, erheben wir folgende Daten:
-          Vor- und Nachname Adresse ggf. Telefonnummer E-Mail-Adresse
-          Zahlungsdaten bestellte Produkte individuelle Personalisierungswünsche
-          (wenn angegeben) Diese Daten werden zur Vertragserfüllung, Lieferung
-          und Kundenkommunikation verwendet.
-        </p>
-      </div>
-      <div className="space-y-2 md:space-y-4">
-        <h2>5. Weitergabe von Daten</h2>
-        <p>
-          Deine Daten werden nur an Dritte weitergegeben, soweit dies zur
-          Abwicklung des Vertrages notwendig ist (z.B. Versanddienstleister,
-          Zahlungsdienstleister). Eine darüber hinausgehende Weitergabe erfolgt
-          nicht.
-        </p>
-      </div>
-      <div className="space-y-2 md:space-y-4">
-        <h2>6. Zahlungsdienstleister</h2>
-        <p>
-          Je nach gewählter Zahlungsart erfolgt die Datenverarbeitung ggf. durch
-          externe Anbieter (z.B. PayPal, Klarna, Stripe). Es gelten deren
-          Datenschutzrichtlinien.
-        </p>
-      </div>
-      <div className="space-y-2 md:space-y-4">
-        <h2>7. Cookies</h2>
-        <p>
-          Unsere Website verwendet Cookies, um die Nutzerfreundlichkeit zu
-          verbessern. Du kannst die Verwendung von Cookies in deinen
-          Browsereinstellungen steuern.
-        </p>
-      </div>
-      <div className="space-y-2 md:space-y-4">
-        <h2>8. Betroffenenrechte (DSGVO)</h2>
-        <p>
-          Du hast jederzeit das Recht auf: Auskunft über deine gespeicherten
-          Daten Berichtigung unrichtiger Daten Löschung („Recht auf
-          Vergessenwerden“) Einschränkung der Verarbeitung Datenübertragbarkeit
-          Widerspruch gegen die Verarbeitung Du kannst dich jederzeit unter der
-          oben genannten Adresse an uns wenden.
-        </p>
-      </div>
-      <div className="space-y-2 md:space-y-4">
-        <h2>9. Beschwerderecht bei der Aufsichtsbehörde</h2>
-        <p>
-          Du hast das Recht, dich bei einer Datenschutz-Aufsichtsbehörde über
-          die Verarbeitung deiner personenbezogenen Daten zu beschweren.
-        </p>
-      </div>
-    </div>
+    </Section>
   )
 }
